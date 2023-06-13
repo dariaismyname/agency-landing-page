0f4c0137b01987e2472f95930f1c0ccc5f3847cb
Daria <daria.cherkasova.work@gmail.com>
2023-06-13 14:42:26 +0300

    finish test project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import CreativeDesign from './components/CreativeDesign'
+import Experience from './components/Experience/Experience'
+import ContactUs from './components/ContactUs'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <CreativeDesign />
+      <Experience />
+      <ContactUs />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
